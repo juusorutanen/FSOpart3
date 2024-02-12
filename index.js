51f75e9da778f57dce5e46ec90560a030730a60a
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-02-12 17:25:27 +0200

    add 3.3 step 3

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-	
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-let persons	= [
-{
-id: 1,
-name:"Arto Hellas",
-number:	"040-123456"
-},
-{
-id: 2,
-name:"Ada Lovelace",
-number:	"39-44-5323523"
-},
-{
-id: 3,
-name:"Dan Abramov",
-number:	"12-43-234345"
-},
-{
-id: 4,
-name:"Mary Poppendieck",
-number:	"39-23-6423122"
-}
-]
-
-app.get('/api/persons', (request,response)=> {
-    response.json(persons)
-  })
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
 // calculate how many people are in the persons array
-app.get('/api/info', (request,response)=> {
-    let amountOfPersons = persons.length
-    response.send(`<h1>Phonebook has info for ${amountOfPersons} people</h1>`)
-    
-  })
+app.get("/api/info", (request, response) => {
+  let amountOfPersons = persons.length;
+  response.send(`<h1>Phonebook has info for ${amountOfPersons} people</h1>`);
+});
 
-const PORT = 3001
+// show persons phone number
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  console.log(person);
+  if (person) {
+    response.send(person.number);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
